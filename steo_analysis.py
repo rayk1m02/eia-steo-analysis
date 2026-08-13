727c46cd6160b62f24ab7fad22435fac94cd95a2
rayk1m02 <hkapray@gmail.com>
2026-08-12 22:42:25 -0500

    successfully retrieve crude data via API

--- steo_analysis.py
+++ steo_analysis.py
@@ -6,3 +6,29 @@ import os
 
 load_dotenv()
 api_key = os.getenv("EIA_TOKEN")
+
+url = "https://api.eia.gov/v2/steo/data/"
+'''
+Filter conditions:
+data[] - value (as seen in chart data)
+facets[seriesID] - Crude oil production in Eagle Ford, Permian, and US
+frequency - Monthly
+sort by most recent dates (desc)
+max rows 5000
+'''
+params = {
+  "api_key": api_key,
+  "data[]": "value",
+  "facets[seriesId][]": ["COPREF", "COPRPM", "COPRPUS"],
+  "frequency": "monthly",
+  "sort[0][column]": "period",
+  "sort[0][direction]": "desc",
+  "length": 5000,
+}
+
+response = requests.get(url, params=params)
+response.status_code
+
+data = response.json()
+data
+# %%
