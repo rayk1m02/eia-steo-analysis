507cce3b0c8eb4a5d8a09773df8aff6a57293407
rayk1m02 <hkapray@gmail.com>
2026-08-13 19:03:29 -0500

    create dataframe and convert dtypes

--- steo_analysis.py
+++ steo_analysis.py
@@ -36,7 +36,7 @@ data
 data.keys()
 data["request"]
 data["response"].keys()
-# data["response"]["data"]
+data["response"]["data"]
 # data["response"]["total"]
 
 '''
@@ -44,8 +44,7 @@ DATA PLAN
 - We have a list of data points, each containing the YYYY-MM, Location, Crude Volume, Unit (million bbls/day)
 - These datapoints span from 1990-01 up to 2027-12
 - OBJECTIVES/LEARNINGS:
-  - convert dtypes to datetime, numeric, etc as needed
-  - create a DataFrame and manipulate it to display clean data
+  - create a DataFrame and convert dtypes to datetime, numeric, etc as needed
   - analysis against US total crude production (%)
     - what % does the Permian represent and similarly, Eagle Ford? What about combined?
     - compute year-over-year growth rate per region. .pct_change()
@@ -58,4 +57,36 @@ DATA PLAN
   - line chart / graph that displays crude production volume across the three regions
   - export findings to csv file
   - refactor code into functions, make this a real reusable pipeline (at the basic level)
+'''
+
+''' create a DataFrame and convert dtypes to datetime, numeric, etc as needed '''
+datapoints = data["response"]["data"]
+type(datapoints)
+datapoints
+{key: type(value) for key, value in datapoints[0].items()}
+
+# {'period': str,
+# 'seriesId': str,
+# 'seriesDescription': str,
+# 'value': str,
+# 'unit': str}
+
+# pd DataFrames expect a list of dicts as one of its stsandard inputs. each dict becoming a row and the keys the columns.
+df_steo = pd.DataFrame(datapoints)
+df_steo
+df_steo["period"] = pd.to_datetime(df_steo["period"])
+df_steo["value"] = pd.to_numeric(df_steo["value"])
+df_steo.dtypes
+
+# period               datetime64[us]
+# seriesId                        str
+# seriesDescription               str
+# value                       float64
+# unit                            str
+# dtype: object
+
+''' 
+analysis against US total crude production (%) 
+  what % does the Permian represent and similarly, Eagle Ford? What about combined?
+  compute year-over-year growth rate per region. .pct_change()
 '''
