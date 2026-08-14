ed5ad5dc3e7da6005b61b58c8718836d117bfc24
rayk1m02 <hkapray@gmail.com>
2026-08-13 23:44:36 -0500

    use pivot table to start building out our % analysis table

--- steo_analysis.py
+++ steo_analysis.py
@@ -96,18 +96,27 @@ df_steo.dtypes
 
 ''' 
 analysis against US total crude production (%) 
-  year by year what was the % of Permian and Eagle Ford against US Crude Production? individually and combined?
-  compute year-over-year growth rate per region. .pct_change()
+  A) year by year what was the % of Permian and Eagle Ford against US Crude Production? individually and combined?
+  B) compute year-over-year growth rate per region. .pct_change()
 '''
-# output example:
+# desired output for part A:
 # Year PermianVolume EagleFordVolume USVolume PermianVolumePct EagelFordVolumePct CombinedPct
 # 2027
 # 2026
 # 2025
-
-# create a new DataFrame called df_steo_pct
-# group by the year of each Date, ID in df_steo only for COPREF, COPRPM. then use pivot
-# then we obtain the Average of the Value field 
-# append a group by US crude Average Value
-# then append column what we caluclated earlier for EF and PM each divided by the US crude Average value
-# and lastly append column of both EF and PM over US Crude Average value
+df_yearly = df_steo.copy()                                        
+df_yearly.insert(                                                 # extract and add the Year as as column next to Date
+  loc=df_yearly.columns.get_loc("Date")+1,
+  column="Year",
+  value=df_yearly["Date"].dt.year
+)
+df_yearly                                                         
+df_tx = df_yearly[df_yearly["ID"].isin(["COPREF", "COPRPM"])]    # grab only Eagle Ford and Permian
+df_tx
+df_steo_pct = df_tx.pivot_table(                                 # This builds out the first three columns of our desired dataframe (to some degree)
+  values="Value",                                                # ID   COPREF  COPRPM
+  index="Year",                                                  # Year
+  columns="ID",                                                  # YYYY    x      y
+  aggfunc="mean"
+  )
+df_steo_pct
